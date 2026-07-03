59c6b86b1820ea0dc640f0a80130ac63945bd41a
Deshmukh Deven Annasaheb <devendeshmukh192007@outlook.com>
2026-07-03 10:45:55 +0530

    Day 5: given num contain 0 at second position

---
+++ Week0/Day5/Problem1.java
@@ -0,0 +1,24 @@
+package Week0.Day5;
+
+import java.util.*;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean ans = isZero(n);
+        System.out.println("Is " + n + " contain 0 at second position:-" + ans);
+    }
+
+    static boolean isZero(int n) {
+        return (abs(n) / 10) % 10 == 0;
+    }
+
+    static int abs(int n) {
+        if (n < 0) {
+            return -n; 
+        } else {
+            return n; 
+        }
+    }
+}
